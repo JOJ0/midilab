972feb56bbce1b850c28e120e6d8f69c8cf3023f
J0J0 T <jojo@peek-a-boo.at>
2019-09-16 16:59:26 +0200

    save with commented out stuff for now

--- midilab.ino
+++ midilab.ino
@@ -1,23 +1,27 @@
-#define p Serial.print
-#define pln Serial.println
+//#define p Serial.print
+//#define pln Serial.println
 
-const int PIN_LED = 13;
+//const int PIN_LED = 13;
 
 // pot
-const int PIN_POT = A0;
-int potVal = 0;
-int ccNum = 21
+//const int PIN_POT = A0;
+//int potVal = 0;
+//int ccPotNum = 21;
+
 // switch
+const int PIN_SW1 = 2;
 bool swVal = false; // or just 0, it's the same
-int ccNum = 101
+int ccSwNum = 101;
 
 void setup() {
-  while(!Serial); // wait until Serial is accessible
-  Serial.begin(9600); // common serial rate
-  //Serial.begin(31250); // MIDI serial rate
+    pinMode(PIN_SW1, INPUT);
+    //while(!Serial); // wait until Serial is accessible
+    //Serial.begin(9600); // common serial rate
+    //Serial.begin(31250); // MIDI serial rate
 }
 
 void loop() {
-
-
+    if (digitalRead(PIN_SW1) == HIGH) {
+        Serial.println("button pressed");
+    }
 }
