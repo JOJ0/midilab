a424faefa9f610d2122738a32e17cc5ab65c7fe4
J0J0 T <jojo@peek-a-boo.at>
2019-09-16 16:59:41 +0200

    remove clutter

--- midilab.ino
+++ midilab.ino
@@ -1,13 +1,3 @@
-//#define p Serial.print
-//#define pln Serial.println
-
-//const int PIN_LED = 13;
-
-// pot
-//const int PIN_POT = A0;
-//int potVal = 0;
-//int ccPotNum = 21;
-
 // switch
 const int PIN_SW1 = 2;
 bool swVal = false; // or just 0, it's the same
@@ -15,9 +5,6 @@ int ccSwNum = 101;
 
 void setup() {
     pinMode(PIN_SW1, INPUT);
-    //while(!Serial); // wait until Serial is accessible
-    //Serial.begin(9600); // common serial rate
-    //Serial.begin(31250); // MIDI serial rate
 }
 
 void loop() {
