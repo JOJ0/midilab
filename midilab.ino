a14eb79253ec428675e477e2c885c6f3f10fb892
J0J0 T <jojo@peek-a-boo.at>
2019-09-17 15:23:55 +0200

    send out CC on or off via serial

--- midilab.ino
+++ midilab.ino
@@ -6,20 +6,27 @@ const byte PIN_SW1 = 2;
 bool swVal = LOW; // or just 0, it's the same
 bool swValPrevious = LOW;
 const int ccSwNum = 101;
+const byte midiCmdCC = 176; // this is a MIDI CC on channel 1
 
 void setup() {
     pinMode(PIN_SW1, INPUT);
-    Serial.begin(9600);
+    Serial.begin(31250);
 }
 
 void loop() {
     swVal = digitalRead(PIN_SW1);
     if (swVal != swValPrevious) {
         if (swVal == HIGH) {
-            p("Sending out MIDI CC on "); pln(ccSwNum);
+            //p("Sending out MIDI CC _on_ "); pln(ccSwNum);
+            Serial.write(midiCmdCC);
+            Serial.write(ccSwNum);
+            Serial.write(127);
         }
         else {
-            p("Sending out MIDI CC off "); pln(ccSwNum);
+            //p("Sending out MIDI CC _off_ "); pln(ccSwNum);
+            Serial.write(midiCmdCC);
+            Serial.write(ccSwNum);
+            Serial.write(0);
         }
     }
     swValPrevious = swVal;
