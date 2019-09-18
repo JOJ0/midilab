2341d159cfd79cf7d36ab147d809c944b6615084
J0J0 T <jojo@peek-a-boo.at>
2019-09-18 14:25:09 +0200

    4 switches loop example

--- midilab.ino
+++ midilab.ino
@@ -2,32 +2,38 @@
 #define pln Serial.println
 
 // switch
-const byte PIN_SW1 = 2;
-bool swVal = LOW; // or just 0, it's the same
-bool swValPrevious = LOW;
-const int ccSwNum = 101;
+const byte SWITCH_PINS[4] = {2,3,4,5};
+bool swVal[4] = {LOW,LOW,LOW,LOW}; // or just 0, it's the same
+bool swValPrevious[4] = {LOW,LOW,LOW,LOW};
+const int ccSwNum[4] = {101,102,103,104};
 const byte midiCmdCC = 176; // this is a MIDI CC on channel 1
 
 void setup() {
-    pinMode(PIN_SW1, INPUT);
-    Serial.begin(31250);
+    pinMode(SWITCH_PINS[0], INPUT);
+    pinMode(SWITCH_PINS[1], INPUT);
+    pinMode(SWITCH_PINS[2], INPUT);
+    pinMode(SWITCH_PINS[3], INPUT);
+    Serial.begin(31250); // this is the MIDI bitrate
+    //Serial.begin(9600); // this is a common serial bitrate (debugging)
 }
 
 void loop() {
-    swVal = digitalRead(PIN_SW1);
-    if (swVal != swValPrevious) {
-        if (swVal == HIGH) {
-            //p("Sending out MIDI CC _on_ "); pln(ccSwNum);
-            Serial.write(midiCmdCC);
-            Serial.write(ccSwNum);
-            Serial.write(127);
+    for (byte swNumber = 0; swNumber < 4; swNumber++) { 
+        swVal[swNumber] = digitalRead(SWITCH_PINS[swNumber]);
+        if (swVal[swNumber] != swValPrevious[swNumber]) {
+            if (swVal[swNumber] == HIGH) {
+                //p("Sending out MIDI CC _on_ "); pln(ccSwNum[swNumber]);
+                Serial.write(midiCmdCC);
+                Serial.write(ccSwNum[swNumber]);
+                Serial.write(127);
+            }
+            else {
+                //p("Sending out MIDI CC _off_ "); pln(ccSwNum[swNumber]);
+                Serial.write(midiCmdCC);
+                Serial.write(ccSwNum[swNumber]);
+                Serial.write(0);
+            }
         }
-        else {
-            //p("Sending out MIDI CC _off_ "); pln(ccSwNum);
-            Serial.write(midiCmdCC);
-            Serial.write(ccSwNum);
-            Serial.write(0);
-        }
+        swValPrevious[swNumber] = swVal[swNumber];
     }
-    swValPrevious = swVal;
 }
